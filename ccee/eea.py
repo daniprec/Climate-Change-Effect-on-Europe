775f428af55775a324a3aeef43b5323e62e76b49
Daniel Precioso <danipg1409@gmail.com>
2025-07-17 12:05:27 +0200

    Refactor EEA air quality data functions for improved processing and clarity
    - Renamed download_eea_air_quality_by_dataset to download_and_process_eea_air_quality_from_API for better clarity.
    - Introduced a new function, process_eea_air_quality_data, to handle data processing after downloading.
    - Updated the main download function to utilize the new processing function and improved logging.
    - Ensured consistent DataFrame handling throughout the data processing steps.

--- ccee/eea.py
+++ ccee/eea.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 DICT_POLLUTANTS = {5: "pm10", 7: "O3", 9: "NOx"}
 
 
-def download_eea_air_quality_by_dataset(
+def download_and_process_eea_air_quality_from_API(
     path_data: str = "./data/",
     nuts_id: str = "AT",
     agg_type: str = "hour",
@@ -123,31 +123,56 @@ def download_eea_air_quality_by_dataset(
     # Concatenate the Arrow tables efficiently
     merged_table = pa.concat_tables(dfs, promote_options="default")
     merged_df = merged_table.to_pandas()
+    return merged_df
+
+
+def process_eea_air_quality_data(
+    df: pd.DataFrame, verbose: bool = True
+) -> pd.DataFrame:
+    """Process the EEA air quality data for a specific NUTS region.
+    This function filters, aggregates, and formats the data for further analysis.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame containing the EEA air quality data, as downloaded from the EEA API.
+    verbose : bool, optional
+        If True, print additional information during processing.
+
+    Returns
+    -------
+    pd.DataFrame
+        Processed DataFrame containing averaged pollutant data for the specified NUTS region.
+    """
+    # Extract the aggregation type from the DataFrame
+    agg_type = df["AggType"].iloc[0]
 
     # Keep only valid rows
-    mask_valid = merged_df["Validity"] == 1
-    merged_df = merged_df[mask_valid].reset_index(drop=True)
+    mask_valid = df["Validity"] == 1
+    df = df[mask_valid].reset_index(drop=True)
     if verbose:
         num_non_valid = (~mask_valid).sum()
         print(
-            f"[INFO] EEA - {nuts_id} - dataset {dataset} - {agg_type} - "
-            f"Removed {num_non_valid} non-valid rows out of {len(merged_df)} total rows."
+            f"[INFO] EEA - {agg_type} - "
+            f"Removed {num_non_valid} non-valid rows out of {len(df)} total rows."
         )
 
     # The letters before "/" in each element in "Samplingpoint" is the NUTS # code. For instance "BA/SPO-BA0038A_00009_100" -> "BA"
-    merged_df["NUTS_ID"] = merged_df["Samplingpoint"].str.split("/").str[0]
+    df["NUTS_ID"] = df["Samplingpoint"].str.split("/").str[0]
+    # TODO: Find where each sampling point is located for higher resolution
+    # Example: AT/SPO.06.107.3804.7.1
 
     # The "Start" column has the format "2024-01-01 00:00:00"
     # We extract from them "Year" and "Week" using pandas after conversion
-    merged_df["Start"] = pd.to_datetime(merged_df["Start"])
-    merged_df["Year"] = merged_df["Start"].dt.year
-    merged_df["Week"] = merged_df["Start"].dt.isocalendar().week
+    df["Start"] = pd.to_datetime(df["Start"])
+    df["Year"] = df["Start"].dt.year
+    df["Week"] = df["Start"].dt.isocalendar().week
     # Turn None in "Unit" to "Unknown" to avoid issues later (with the groupby)
-    merged_df["Unit"] = merged_df["Unit"].fillna("Unknown")
+    df["Unit"] = df["Unit"].fillna("Unknown")
 
     # Group by NUTS_ID, Year, Week, Pollutant. Average the Value
-    merged_df = (
-        merged_df.groupby(
+    df = (
+        df.groupby(
             ["NUTS_ID", "Year", "Week", "Pollutant", "Unit", "AggType", "Verification"],
             as_index=False,
         )
@@ -158,48 +183,46 @@ def download_eea_air_quality_by_dataset(
     # Extra check: ensure the is a single "Unit" and "AggType" per NUTS_ID, Year, Week, Pollutant
     for col in ["Unit", "AggType", "Verification"]:
         if (
-            merged_df.groupby(["NUTS_ID", "Year", "Week", "Pollutant"])[col].nunique()
-            > 1
+            df.groupby(["NUTS_ID", "Year", "Week", "Pollutant"])[col].nunique() > 1
         ).any():
             print(
-                f"[WARNING] EEA - {nuts_id} - dataset {dataset} - {agg_type} - "
+                f"[WARNING] EEA - {agg_type} - "
                 f"Multiple unique values found in {col} for some combinations."
-                f"{merged_df[col].unique()}"
+                f"{df[col].unique()}"
             )
             # Average the values in case of multiple unique values
-            merged_df[col] = merged_df.groupby(
-                ["NUTS_ID", "Year", "Week", "Pollutant"]
-            )[col].transform("first")
+            df[col] = df.groupby(["NUTS_ID", "Year", "Week", "Pollutant"])[
+                col
+            ].transform("first")
     # Drop these columns as they are not needed in the final output
-    merged_df.drop(columns=["Unit", "AggType", "Verification"], inplace=True)
+    df.drop(columns=["Unit", "AggType", "Verification"], inplace=True)
 
     # Sort by NUTS_ID, Year, Week, Pollutant
-    merged_df = merged_df.sort_values(
-        by=["NUTS_ID", "Year", "Week", "Pollutant"], ignore_index=True
-    )
+    df = df.sort_values(by=["NUTS_ID", "Year", "Week", "Pollutant"], ignore_index=True)
 
     # Convert pollutant numbers to names
-    merged_df["Pollutant"] = (
-        merged_df["Pollutant"].map(DICT_POLLUTANTS).fillna("Unknown")
-    )
+    df["Pollutant"] = df["Pollutant"].map(DICT_POLLUTANTS).fillna("Unknown")
 
     # Rename columns to match the rest of the project
-    merged_df.rename(columns={"Year": "year", "Week": "week"}, inplace=True)
+    df.rename(columns={"Year": "year", "Week": "week"}, inplace=True)
 
     # Pivot the table:
     # Columns: NUTS_ID, year, week, pollutant names (from "Pollutant")
     # Value in each pollutant cell is the old "Value" column
-    merged_df = merged_df.pivot_table(
+    df = df.pivot_table(
         index=["NUTS_ID", "year", "week"],
         columns="Pollutant",
         values="Value",
         aggfunc="mean",
     ).reset_index()
 
-    return merged_df
+    # Sort the DataFrame by NUTS_ID, year, week
+    df.sort_values(by=["NUTS_ID", "year", "week"]).reset_index(drop=True)
+
+    return df
 
 
-def download_eea_air_quality(
+def download_and_process_eea_air_quality(
     path_data: str = "./data/",
     nuts_id: str = "AT",
     agg_type: str = "hour",
@@ -234,7 +257,7 @@ def download_eea_air_quality(
     # (3) Historical Airbase data delivered between 2002 and 2012 before Air
     # Quality Directive 2008/50/EC entered into force
     for dataset in [1, 2, 3]:
-        df = download_eea_air_quality_by_dataset(
+        df = download_and_process_eea_air_quality_from_API(
             path_data=path_data,
             nuts_id=nuts_id,
             agg_type=agg_type,
@@ -244,6 +267,8 @@ def download_eea_air_quality(
         ls_df.append(df)
     # Concatenate all DataFrames
     df = pd.concat(ls_df, ignore_index=True)
+    # Process the concatenated DataFrame
+    df = process_eea_air_quality_data(df, verbose=verbose)
     return df
 
 
@@ -334,8 +359,7 @@ def download_tif_from_eea_datastore_folder(folder_url: str, path_data: str = "./
 
 def main(download_tif: bool = False):
     if not download_tif:
-        df = download_eea_air_quality(verbose=True)
-        df = df.sort_values(by=["NUTS_ID", "year", "week"]).reset_index(drop=True)
+        df = download_and_process_eea_air_quality(verbose=True)
         print(df.head())
         print(df.tail())
     else:
--- scripts/build_csv.py
+++ scripts/build_csv.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 from ccee.cordex import cordex_tas_to_dataframe_per_region
-from ccee.eea import download_eea_air_quality
+from ccee.eea import download_and_process_eea_air_quality
 from ccee.eurostat import (
     download_eurostat_mortality,
     download_eurostat_nuts2_population,
@@ -72,7 +72,7 @@ def main(path_data: str = "./data", path_geojson: str = "./data/regions.geojson"
     for nuts_id in df["NUTS_ID"].unique():
         # Download the air quality data for the specified pollutant and NUTS_ID
         print(f"[INFO] Downloading air quality data for NUTS_ID {nuts_id}...")
-        df_aq = download_eea_air_quality(
+        df_aq = download_and_process_eea_air_quality(
             path_data=path_data, nuts_id=nuts_id, verbose=True
         )
         ls_df_aq.append(df_aq)
